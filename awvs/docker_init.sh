4ca53d0f20bf8213ea3b413d80f66b998833386d
kzaopa <kzaopa@gmail.com>
2022-06-01 09:29:36 +0800

    update awvs

---
+++ awvs/docker_init.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# set -ex
+
+clear
+OS="$(uname -s)"
+Arch="$(uname -m)"
+Tag="$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)"
+
+DockerInstall(){
+    echo " ╷───────────────────────────╷"
+    echo " │    OS ${OS}              │"
+    echo " │    Arch ${Arch}             │"
+    echo " │    Docker Version: ${Tag} │"
+    echo " ╵–––––––––––––––––––––––––––╵"
+    case "${OS}" in
+        Linux)
+            echo " Install Docker for Linux"
+            curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+            sudo systemctl enable docker
+            sudo systemctl start docker
+            sudo usermod -aG docker "$USER"
+        ;;
+        Darwin)
+            echo " Docker Error: Docker Desktop https://www.docker.com/products/docker-desktop"
+            if [ "${OS}" == "Darwin" ] && [ "${Arch}" == "arm64" ]; then
+                Arch="aarch64"
+            fi
+        ;;
+        *)
+        echo " Unknown OS: ${OS}}"
+        exit 1
+        ;;
+    esac
+}
+
+DockerComposeInstall(){
+    echo " Downloading docker-compose..."
+    sudo curl -sLm 10 "https://ghproxy.com/https://github.com/docker/compose/releases/download/${Tag}/docker-compose-${OS}-${Arch}" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+}
+
+DockerInstall
+
+DockerComposeInstall
